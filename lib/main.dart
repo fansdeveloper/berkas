a8f9421dc4c8d1b88381ecf147c53de84882f51d
natasyadlulu28 <natasyadlulu@gmail.com>
2020-12-29 19:19:53 +0700

    Logo BerKas

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,9 @@
 import 'package:berkas/ui/pages/pages.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  SharedPreferences prefs = await SharedPreferences.getInstance();
   await Firebase.initializeApp();
   runApp(MyApp());
 }
